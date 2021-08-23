eb107ca26aa4c14346f55dc4b5ab26bf7bad189f
James Scott-Brown <james@jamesscottbrown.com>
2021-08-24 00:04:42 +0100

    automatically migrate older trrrace.json files

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,19 @@ import './App.global.css';
 
 import { useProjectState } from './components/ProjectContext';
 
+const migrateTrrraceFormat = (projectData) => {
+  // - add url array if not already present
+  // - convert tags list on entry from object to string
+  return {
+    ...projectData,
+    entries: projectData.entries.map((e) => ({
+      ...e,
+      urls: e.urls ? e.urls : [],
+      tags: e.tags.map((t) => (typeof t === 'string' ? t : t.text)),
+    })),
+  };
+};
+
 export default function App() {
   const [folderPath, setPath] = useState<string>('');
 
@@ -34,9 +47,8 @@ export default function App() {
         dispatch({
           type: 'SET_DATA',
           folderName,
-          projectData: JSON.parse(data),
+          projectData: migrateTrrraceFormat(JSON.parse(data)),
         });
-        console.log(data);
       }
     });
   });
