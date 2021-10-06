1757f4095f30fa9a193f431479326ab37b2060b2
Jen Rogers <jen@sci.utah.edu>
2021-10-05 18:01:01 -0600

    ability to write python file

--- nlp-python/app.py
+++ nlp-python/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 import os
 import sys
+import json
 
 app = Flask(__name__)
 
@@ -12,11 +13,34 @@ def filter_txt(file):
 def index():
     document_path = '/Volumes/GoogleDrive/Shared drives/trrrace/Derya Artifact Trrracer/'
     files = os.listdir(document_path)
-    
     txt_files = filter_txt(files)
 
-    for t in txt_files:
-        f = open(document_path + t, 'r')
-        print(f.read())
+    data_backbone = open(document_path + "trrrace.json", 'r')
+    d_b_json = json.loads(data_backbone.read())
+
+    # for t in txt_files:
+    #     f = open(document_path + t, 'r')
+    #     print(f.read())
+
+    for en in d_b_json["entries"]:
+        # print(en["title"])
+        for f in en["files"]:
+            # print(f["title"])
+            if "fileType" not in f:
+                print(f["title"])
+                if "txt" in f["title"]:
+                    f["fileType"] = "txt"
+                elif "gdoc" in f["title"]:
+                    f["fileType"] = "gdoc"
+                elif "pdf" in f["title"]:
+                    f["fileType"] = "pdf"
+
+    
+    write_path = '/Volumes/GoogleDrive/Shared drives/trrrace/Derya Artifact Trrracer/trrracePYTHON.json'
+
+    with open(write_path, 'w') as outfile:
+        json.dump(d_b_json, outfile)
+            
+
         
-    return str(txt_files)
+    return str(d_b_json["entries"])
