e142760da2b4d0ee4ea1a4d86df431f735e158b6
Jen Rogers <jen@sci.utah.edu>
2021-10-06 10:23:05 -0600

    added google api

--- nlp-python/app.py
+++ nlp-python/app.py
@@ -2,16 +2,26 @@ from flask import Flask
 import os
 import sys
 import json
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google_api import goog_auth, goog_doc_start, get_doc_text_by_id
 
 app = Flask(__name__)
 
 def filter_txt(file):
     return [f for f in file if "txt" in f]
 
-
 @app.route("/")
 def index():
     document_path = '/Volumes/GoogleDrive/Shared drives/trrrace/Derya Artifact Trrracer/'
+
+    cred = goog_auth()
+    gdoc_service = goog_doc_start(cred)
+
+     # Retrieve the documents contents from the Docs service.
+    get_doc_text_by_id(gdoc_service, '1rl5X_PwEdSkXXzo-afV1B-Ig86ZDJJcXmyKyFp5h8WY')
+
+   
+
     files = os.listdir(document_path)
     txt_files = filter_txt(files)
 
@@ -38,8 +48,8 @@ def index():
     
     write_path = '/Volumes/GoogleDrive/Shared drives/trrrace/Derya Artifact Trrracer/trrrace.json'
 
-    # with open(write_path, 'w') as outfile:
-    #     json.dump(d_b_json, outfile)
+    with open(write_path, 'w') as outfile:
+        json.dump(d_b_json, outfile)
             
 
         
---
+++ nlp-python/google_api.py
@@ -0,0 +1,86 @@
+from __future__ import print_function
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/drive']
+
+# The ID of a sample document.
+DOCUMENT_ID = '195j9eDD3ccgjQRttHhJPymLJUCOUjs-jmwTrekvdjFE'
+
+def goog_auth():
+    """Shows basic usage of the Docs API.
+    Prints the title of a sample document.
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token-py.json'):
+        creds = Credentials.from_authorized_user_file('token-py.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                '../assets/google_cred_desktop_app.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token-py.json', 'w') as token:
+            token.write(creds.to_json())
+
+    return creds
+
+def goog_doc_start(creds):
+
+    service = build('docs', 'v1', credentials=creds)
+
+    return service
+
+def read_paragraph_element(element):
+    """Returns the text in the given ParagraphElement.
+
+        Args:
+            element: a ParagraphElement from a Google Doc.
+    """
+    text_run = element.get('textRun')
+    if not text_run:
+        return ''
+    return text_run.get('content')
+
+
+def read_strucutural_elements(elements):
+    """Recurses through a list of Structural Elements to read a document's text where text may be
+        in nested elements.
+
+        Args:
+            elements: a list of Structural Elements.
+    """
+    text = ''
+    for value in elements:
+        if 'paragraph' in value:
+            elements = value.get('paragraph').get('elements')
+            for elem in elements:
+                text += read_paragraph_element(elem)
+        elif 'table' in value:
+            # The text in table cells are in nested Structural Elements and tables may be
+            # nested.
+            table = value.get('table')
+            for row in table.get('tableRows'):
+                cells = row.get('tableCells')
+                for cell in cells:
+                    text += read_strucutural_elements(cell.get('content'))
+        elif 'tableOfContents' in value:
+            # The text in the TOC is also in a Structural Element.
+            toc = value.get('tableOfContents')
+            text += read_strucutural_elements(toc.get('content'))
+    return text
+
+def get_doc_text_by_id(docs_service, id):
+    doc = docs_service.documents().get(documentId=id).execute()
+    doc_content = doc.get('body').get('content')
+    print(read_strucutural_elements(doc_content))
