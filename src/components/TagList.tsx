153727080163d86dcbd43538314cc9f8c5b736bd
James Scott-Brown <james@jamesscottbrown.com>
2021-11-01 13:39:13 +0000

    represent tags with MenuButtons - more compact

--- src/components/TagList.tsx
+++ src/components/TagList.tsx
@@ -1,13 +1,197 @@
 import React, { useState } from 'react';
-import { Grid, Heading } from '@chakra-ui/react';
+import {
+  Button,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+} from '@chakra-ui/react';
 
-import EdiText from 'react-editext';
+import { FaPencilAlt, FaTrashAlt } from 'react-icons/fa';
 import { MdColorLens } from 'react-icons/md';
-import { FaTrashAlt } from 'react-icons/fa';
-import { ColorResult, GithubPicker } from 'react-color';
+import { ChevronDownIcon } from '@chakra-ui/icons';
+
+import { GithubPicker } from 'react-color';
 
 import { useProjectState } from './ProjectContext';
 import { EntryType, TagType } from './types';
+
+interface ColorChangeModalProps {
+  tagToChangeColor: number;
+  setTagToChangeColor: (tagToRename: false | number) => void;
+  tags: TagType[];
+}
+
+const ColorChangeModal = (props: ColorChangeModalProps) => {
+  const { tagToChangeColor, setTagToChangeColor, tags } = props;
+  const [newColor, setNewColor] = useState<string>(
+    tags[tagToChangeColor].color
+  );
+  const [, dispatch] = useProjectState();
+
+  const updateTagColor = (color: string) => {
+    dispatch({
+      type: 'UPDATE_TAG_COLOR',
+      tagIndex: tagToChangeColor,
+      color,
+    });
+    setTagToChangeColor(false);
+  };
+
+  const newColorIsDifferent = newColor !== tags[tagToChangeColor].color;
+
+  return (
+    <>
+      <Modal isOpen onClose={() => setTagToChangeColor(false)}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>
+            Change color for tag {tags[tagToChangeColor].title}
+          </ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <div
+              style={{
+                marginTop: '10px',
+                marginBottom: '10px',
+                marginLeft: 'auto',
+                marginRight: 'auto',
+                width: 'max-content',
+              }}
+            >
+              <GithubPicker
+                triangle="hide"
+                color={newColor}
+                onChangeComplete={(color) => setNewColor(color.hex)}
+              />
+            </div>
+
+            {newColorIsDifferent ? (
+              <p>
+                Change <code>{tags[tagToChangeColor].title}</code> from
+                <span
+                  style={{
+                    color: tags[tagToChangeColor].color,
+                    fontSize: '2em',
+                  }}
+                >
+                  ■
+                </span>
+                to
+                <span
+                  style={{
+                    color: newColor,
+                    fontSize: '2em',
+                  }}
+                >
+                  ■
+                </span>
+                ?
+              </p>
+            ) : (
+              <p>Selected color is the same as the current color.</p>
+            )}
+          </ModalBody>
+
+          <ModalFooter>
+            <Button
+              colorScheme="gray"
+              mr={3}
+              onClick={() => setTagToChangeColor(false)}
+            >
+              Cancel
+            </Button>
+
+            <Button
+              colorScheme="blue"
+              enabled={newColorIsDifferent}
+              mr={3}
+              onClick={() => updateTagColor(newColor)}
+            >
+              Change color
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
+interface RenamingModalProps {
+  tagToRename: false | number;
+  setTagToRename: (tagToRename: false | number) => void;
+  newName: string;
+  setNewName: (newName: string) => void;
+  tags: TagType[];
+}
+
+const RenamingModal = (props: RenamingModalProps) => {
+  const { tagToRename, setTagToRename, newName, setNewName, tags } = props;
+  const [, dispatch] = useProjectState();
+
+  const updateTagName = (tagIndex: number, title: string) => {
+    dispatch({
+      type: 'UPDATE_TAG_NAME',
+      tagIndex,
+      title,
+    });
+    setTagToRename(false);
+  };
+
+  if (tagToRename === false) {
+    return <></>;
+  }
+
+  return (
+    <>
+      <Modal isOpen onClose={() => setTagToRename(false)}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Rename tag {tags[tagToRename].title}</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <FormControl>
+              <FormLabel>New name for tag: </FormLabel>
+              <Input
+                value={newName}
+                onChange={(ev) => setNewName(ev.target.value)}
+              />
+            </FormControl>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button
+              colorScheme="gray"
+              mr={3}
+              onClick={() => setTagToRename(false)}
+            >
+              Cancel
+            </Button>
+
+            <Button
+              colorScheme="blue"
+              mr={3}
+              onClick={() => updateTagName(tagToRename, newName)}
+            >
+              Rename
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
 
 interface TagListProps {
   tags: TagType[];
@@ -19,24 +203,10 @@ const TagList = (props: TagListProps) => {
   const [tagToChangeColor, setTagToChangeColor] =
     useState<false | number>(false);
 
+  const [tagToRename, setTagToRename] = useState<false | number>(false);
+  const [newName, setNewName] = useState<string>('');
+
   const [{ projectData }, dispatch] = useProjectState();
-
-  const updateTagColor = (color: ColorResult) => {
-    dispatch({
-      type: 'UPDATE_TAG_COLOR',
-      tagIndex: tagToChangeColor,
-      color: color.hex,
-    });
-    setTagToChangeColor(false);
-  };
-
-  const updateTagName = (tagIndex: number, title: string) => {
-    dispatch({
-      type: 'UPDATE_TAG_NAME',
-      tagIndex,
-      title,
-    });
-  };
 
   const deleteTag = (tagName: string) => {
     const entriesToModify = projectData.entries.filter((e: EntryType) =>
@@ -56,72 +226,62 @@ const TagList = (props: TagListProps) => {
     <>
       <Heading as="h2">Tags</Heading>
 
-      <div style={{ width: 'fit-content' }}>
-        {tags.map((tag: TagType, i) => (
-          <Grid key={tag.title} templateColumns="20px 20px 1fr 20px">
-            <span
-              style={{
-                color: tag.color,
-                cursor: 'default',
-                display: 'flex',
-                alignItems: 'center',
-              }}
-            >
-              ■
-            </span>
-            <span
-              style={{
-                display: 'flex',
-                alignItems: 'center',
-              }}
-            >
-              {' '}
-              <MdColorLens
-                onClick={() => setTagToChangeColor(i)}
-                style={{ verticalAlign: 'middle' }}
-                title="Change tag color"
-              />
-            </span>
+      <RenamingModal
+        tagToRename={tagToRename}
+        setTagToRename={setTagToRename}
+        newName={newName}
+        setNewName={setNewName}
+        tags={tags}
+      />
 
-            <EdiText
-              type="text"
-              value={tag.title}
-              onSave={(val) => updateTagName(i, val)}
-              validation={(val) => val.length > 0}
-              validationMessage="Tag name must not be empty"
-              editOnViewClick
-              submitOnEnter
-              submitOnUnfocus
-            />
+      {tagToChangeColor !== false && (
+        <ColorChangeModal
+          tagToChangeColor={tagToChangeColor}
+          setTagToChangeColor={setTagToChangeColor}
+          tags={tags}
+        />
+      )}
 
-            <span
-              style={{
-                display: 'flex',
-                alignItems: 'center',
-              }}
-            >
-              <FaTrashAlt
-                onClick={() => deleteTag(tag.title)}
-                title="Delete tag"
-              />
-            </span>
+      <div>
+        <div style={{ width: 'fit-content' }}>
+          {tags.map((tag: TagType, i) => (
+            // <Grid key={tag.title} templateColumns="20px 20px 1fr 20px">
+            <Menu key={tag.title}>
+              <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+                <span
+                  style={{
+                    color: tag.color,
+                    cursor: 'default',
+                    display: 'inline',
+                    // display: 'flex',
+                    // alignItems: 'center',
+                  }}
+                >
+                  ■
+                </span>
+                {tag.title}
+              </MenuButton>
 
-            {tagToChangeColor === i && (
-              <>
-                {' '}
-                <div style={{ marginTop: '10px', marginBottom: '10px' }}>
-                  <GithubPicker
-                    color={tags[tagToChangeColor].color}
-                    onChangeComplete={(color) => updateTagColor(color)}
-                  />
-                </div>
-                <div />
-                <div />
-                <div />
-              </>
-            )}
-          </Grid>
-        ))}
+              <MenuList>
+                <MenuItem onClick={() => setTagToChangeColor(i)}>
+                  <MdColorLens
+                    style={{ verticalAlign: 'middle', display: 'inline' }}
+                    title="Change tag color"
+                  />{' '}
+                  Change color
+                </MenuItem>
+
+                <MenuItem onClick={() => deleteTag(tag.title)}>
+                  <FaTrashAlt title="Delete tag" /> Delete tag
+                </MenuItem>
+
+                <MenuItem onClick={() => setTagToRename(i)}>
+                  <FaPencilAlt title="Rename tag" /> Rename tag
+                </MenuItem>
+              </MenuList>
+            </Menu>
+          ))}
+        </div>
       </div>
     </>
   );
