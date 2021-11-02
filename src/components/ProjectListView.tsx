jamesscottbrown/trrracer

commit 34a88f0b58efbce28518262f5c7319b56208e3a5

ensure new entry is editable when it is created

src/components/ProjectListView.tsx
@@ -21,8 +21,13 @@ const ProjectListView = (ProjectPropValues: ProjectViewProps) => {
   );
 
   useEffect(() => {
-    if (editable.length !== projectData.entries.length) {
-      setEditable(Array.from(Array(projectData.entries.length)));
+    if (editable.length === projectData.entries.length - 1) {
+      // one more entry was added
+      setEditable([...editable, true]);
+    } else {
+      setEditable(
+        Array.from(Array(projectData.entries.length), (_, x) => false)
+      );
     }
   }, [projectData]);
 
@@ -137,5 +142,4 @@ const ProjectListView = (ProjectPropValues: ProjectViewProps) => {
     </div>
   );
 };
-
 export default ProjectListView;

commit 0ad05c681c1956a37f1ba5bb9ab32d9b817d37ce

don't  make entries non-editable when a field is edited
This was a regression introduced in 34a88f0b58e.

src/components/ProjectListView.tsx
@@ -24,7 +24,7 @@ const ProjectListView = (ProjectPropValues: ProjectViewProps) => {
     if (editable.length === projectData.entries.length - 1) {
       // one more entry was added
       setEditable([...editable, true]);
-    } else {
+    } else if (editable.length !== projectData.entries.length) {
       setEditable(
         Array.from(Array(projectData.entries.length), (_, x) => false)
       );