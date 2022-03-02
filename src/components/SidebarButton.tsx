c5ad1847966f607e0ff5f0f900e2da0210702eb7
James Scott-Brown <james@jamesscottbrown.com>
2022-03-02 12:35:11 +0000

    Fix performance issue
    Rather than creating a popover for each element, create a popover
    only if it will be displayed, and manage it's open/closed status
    as a controlled component.

--- src/components/SidebarButton.tsx
+++ src/components/SidebarButton.tsx
@@ -1,79 +1,134 @@
 import React, { useState } from 'react';
 import {
   Box,
-  Flex,
-  Heading,
   Popover,
   PopoverTrigger,
   PopoverArrow,
   PopoverContent,
   PopoverBody,
-  Button
+  Button,
 } from '@chakra-ui/react';
 
-import * as d3 from "d3";
 import { useProjectState } from './ProjectContext';
 
 const SidebarButton = (sidebarProps: any) => {
+  const { isTag, index, data } = sidebarProps;
+  const [{ researchThreads }, dispatch] = useProjectState();
+  const [barColor, setBarColor] = useState('#FFFFFF');
+  const [showThreadPop, setShowThreadPop] = useState(false);
+  const [showPopover, setShowPopover] = useState(false);
 
-    const {isTag, index, data} = sidebarProps;
-    const [{ projectData, researchThreads }, dispatch] = useProjectState();
-    const [barColor, setBarColor] = useState('#FFFFFF');
-    const [showThreadPop, setShowThreadPop] = useState(false);
+  const closePopover = () => {
+    setShowPopover(false);
+    if (isTag) {
+      setBarColor('#FFFFFF');
+      // dispatch({ type: 'HIGHLIGHT_TAG', highlightedTag: null });
+    }
+  };
 
+  if (!isTag) {
     return (
-        isTag ? 
-        <Popover trigger="hover">
-            <PopoverTrigger> 
+      <Box
+        style={{ cursor: 'pointer' }}
+        bg={barColor}
+        key={`${data.title}-${index}`}
+        onMouseOver={() => {
+          setBarColor('#D3D3D3');
+        }}
+        onMouseLeave={() => {
+          setBarColor('#FFFFFF');
+        }}
+      >
+        {`${data.title}  (${data.matches.length})`}
+      </Box>
+    );
+  } else if (!showPopover) {
+    return (
+      <Box
+        style={{ cursor: 'pointer' }}
+        bg={barColor}
+        key={`${data.title}-${index}`}
+        onMouseEnter={() => {
+          setShowPopover(true);
+          if (isTag) {
+            setBarColor('#D3D3D3');
+            dispatch({ type: 'HIGHLIGHT_TAG', highlightedTag: data.title });
+          }
+        }}
+        onMouseLeave={() => {
+          console.log('Moused out:', data.title);
 
-                <Box style={{cursor:"pointer"}} bg={barColor} key={`${data.title}-${index}`} 
-                    onMouseOver={()=> {
-                    if(isTag){
-                        setBarColor('#D3D3D3');
-                        // dispatch({type: "HIGHLIGHT_TAG", highlightedTag: data.title}) 
+          setShowPopover(false);
+          if (isTag) {
+            setBarColor('#FFFFFF');
+            dispatch({ type: 'HIGHLIGHT_TAG', highlightedTag: null });
+          }
+        }}
+      >
+        {`${data.title}  (${data.matches.length})`}
+      </Box>
+    );
+  }
+
+  return (
+    <Popover
+      isOpen={showPopover}
+      onClose={closePopover}
+      onMouseLeave={closePopover}
+    >
+      <PopoverTrigger>
+        <Box
+          style={{ cursor: 'pointer' }}
+          bg={barColor}
+          key={`${data.title}-${index}`}
+        >
+          {`${data.title}  (${data.matches.length})`}
+        </Box>
+      </PopoverTrigger>
+      <PopoverContent bg="white" color="gray">
+        <PopoverArrow bg="white" />
+
+        <PopoverBody>
+          {showThreadPop ? (
+            <Box>
+              {researchThreads &&
+              researchThreads.research_threads.length > 0 ? (
+                researchThreads.research_threads.map((r, tIndex: number) => (
+                  <Box
+                    key={`t-${tIndex}`}
+                    onClick={() =>
+                      dispatch({
+                        type: 'ADD_TAG_TO_THREAD',
+                        tag: data.title,
+                        threadIndex: tIndex,
+                      })
                     }
-                    }} onMouseLeave={() => {
-                        if(isTag){
-                            setBarColor('#FFFFFF');
-                            // dispatch({type: "HIGHLIGHT_TAG", highlightedTag: null})
-                        }
-                    }}>{`${data.title}  (${data.matches.length})`}
-                </Box>    
-            
-            </PopoverTrigger> 
-                <PopoverContent bg='white' color='gray'>
-                <PopoverArrow bg='white' />
-                               
-                <PopoverBody>
-                    {
-                        showThreadPop ? 
-                        <Box>
-                            {
-                                (researchThreads && researchThreads.research_threads.length > 0) ?
-                                researchThreads.research_threads.map((r, tIndex:number)=> (
-                                    <Box key={`t-${tIndex}`} 
-                                        onClick={()=> dispatch({type: "ADD_TAG_TO_THREAD", tag: data.title, threadIndex: tIndex})}
-                                        style={{border:"1px solid gray", borderRadius:"5px", cursor:"pointer", textAlign:"center"}}>
-                                        {`Add to "${r.title}"`}</Box>
-                                ))
-                                : <span>{"no threads yet"}</span>
-                            }
-                            <Button onClick={()=> setShowThreadPop(false)}>{"cancel"}</Button>
-                        </Box> :
-                        <Button onClick={()=> setShowThreadPop(true)}>{"Add this tag to a thread."}</Button>
-                    }
-                </PopoverBody>
-            </PopoverContent>
-        </Popover>
-        : 
-        <Box style={{cursor:"pointer"}} bg={barColor} key={`${data.title}-${index}`} 
-            onMouseOver={()=> {
-                setBarColor('#D3D3D3');
-            }} onMouseLeave={() => {
-                setBarColor('#FFFFFF');
-            }}>{`${data.title}  (${data.matches.length})`}
-        </Box>      
-    )
-}
+                    style={{
+                      border: '1px solid gray',
+                      borderRadius: '5px',
+                      cursor: 'pointer',
+                      textAlign: 'center',
+                    }}
+                  >
+                    {`Add to "${r.title}"`}
+                  </Box>
+                ))
+              ) : (
+                <span>{'no threads yet'}</span>
+              )}
+              <Button onClick={() => setShowThreadPop(false)}>
+                {'cancel'}
+              </Button>
+            </Box>
+          ) : (
+            <Button onClick={() => setShowThreadPop(true)}>
+              {'Add this tag to a thread.'}
+            </Button>
+          )}
+        </PopoverBody>
+      </PopoverContent>
+    </Popover>
+  );
+};
 
 export default SidebarButton;
