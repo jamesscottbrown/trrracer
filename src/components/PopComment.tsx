5cdd6c1b804068142486c549d2c9c8af6b4f1a03
James Scott-Brown <james@jamesscottbrown.com>
2022-03-05 15:53:46 +0000

    function arguments in PopComment

--- src/components/PopComment.tsx
+++ src/components/PopComment.tsx
@@ -12,8 +12,95 @@ import {
   } from '@chakra-ui/react';
 import { format } from 'path/posix';
 
-  import React, { useState, useEffect } from 'react';
-  import { useProjectState } from './ProjectContext';
+import React, { useState, useEffect } from 'react';
+import { useProjectState } from './ProjectContext';
+
+const colorConvert = (codes: Object) => {
+  return `rgb(${255 * codes.red},${255 * codes.green},${255 * codes.blue})`;
+};
+
+const formatText = (blob, spanType) => {
+  if (spanType === 'comment') {
+    return blob['quotedFileContent']
+      ? `${blob['quotedFileContent']['value']}`
+      : ' ';
+  } else {
+    return blob['em']['content'];
+  }
+};
+
+const formatContext = (blob, pos) => {
+  if (blob.context) {
+    return pos === 'before' ? blob.context[0] : blob.context[1];
+  } else {
+    return '';
+  }
+};
+
+const renderPop = (blob, spanType, anonName) => {
+  if (spanType === 'comment') {
+    let starter = formatPopoverComment(blob, anonName);
+    if (blob.replies.length > 0) {
+      let rep = blob.replies.map((m) => formatPopoverComment(m, anonName));
+      starter += '-----------------------------' + rep;
+    }
+    return starter;
+  } else {
+    return formatPopoverEmText(blob);
+  }
+};
+
+const formatPopoverComment = (blob, anonName)=> {
+  return `${anonName(blob["author"]["displayName"], 'initials')} (${anonName(blob["author"]["displayName"], 'role')}): "${blob["content"]}"`
+}
+
+const formatPopoverEmText = (blob) => {
+
+  let blobIndex = blob['index-in'];
+  // let before = (blobIndex > 1 && state.projectData.googleData[blob['google-id']].body.content[blobIndex - 1].paragraph) ? state.projectData.googleData[blob['google-id']].body.content[blobIndex - 1].paragraph.elements : " ";
+  // let after = (state.projectData.googleData[blob['google-id']].body.content[blobIndex + 1] && state.projectData.googleData[blob['google-id']].body.content[blobIndex + 1].paragraph) ? state.projectData.googleData[blob['google-id']].body.content[blobIndex + 1].paragraph.elements : " ";
+  // return `${before[0].textRun} .... ${after.at(-1).textRun}`
+}
+
+const formatEmphasis = (blob, spanType) => {
+  let blobOb = { margin: '4px' };
+  if (spanType === 'comment') {
+    blobOb['fontWeight'] = 'bold';
+    blobOb['backgroundColor'] = 'rgb(253,213,145)';
+  } else {
+    if (blob['em']['textStyle'].bold === true) blobOb['fontWeight'] = 'bold';
+
+    if (blob['em']['textStyle'].backgroundColor)
+      blobOb['backgroundColor'] = colorConvert(blob['em']['textStyle'].backgroundColor.color.rgbColor);
+  }
+  return blobOb;
+};
+
+  const formatEmphasisVis = (blob, spanType) => {
+
+    let blobOb =  {margin: '2px', width:'5px', height:'50px', backgroundColor: 'gray', display:"inline-block"}
+    if (spanType === 'comment') {
+        blobOb["fontWeight"] = "bold";
+        blobOb["backgroundColor"] = "rgb(253,213,145)";
+    } else {
+        if (blob['em']['textStyle'].bold === true) blobOb["width"] = '10px';
+        if (blob['em']['textStyle'].italic === true) blobOb["fontStyle"] = "italic";
+
+        if (blob['em']['textStyle'].backgroundColor) {
+          blobOb["backgroundColor"] = colorConvert(blob['em']['textStyle'].backgroundColor.color.rgbColor)
+          blobOb["borderColor"] = "gray";
+          blobOb["borderStyle"] = "solid";
+          blobOb["borderWidth"] = '1px';
+        }
+        if (blob['em']['textStyle'].foregroundColor){
+          blobOb["backgroundColor"] = colorConvert(blob['em']['textStyle'].foregroundColor.color.rgbColor);
+          blobOb["borderColor"] = "gray";
+          blobOb["borderStyle"] = "solid";
+          blobOb["borderWidth"] = '1px';
+        }
+    }
+    return blobOb
+  }
 
   const PopComment = (props) => {
 
@@ -34,106 +121,13 @@ import { format } from 'path/posix';
             console.log('NAME NOT FOUND', name)
         }
       }
-  
-      const colorConvert = (codes: Object)=> {
-        
-          return `rgb(${(255 * codes.red)},${(255 * codes.green)},${(255 * codes.blue)})`
-        }
-  
-      const formatText = (blob)=> {
-          if(spanType === "comment"){
-            
-            return blob["quotedFileContent"] ? `${blob["quotedFileContent"]["value"]}` : " "
-           
-          }else{
-              return blob['em']["content"]
-          }
-      }
-
-      const formatContext = (blob, pos) => {
-        if(blob.context){
-          return pos === 'before' ? blob.context[0] : blob.context[1]
-        }else{
-          return ""
-        }
-      }
-
-      const renderPop = (blob) => {
-        if(spanType === "comment"){
-
-            let starter = formatPopoverComment(blob);
-            if(blob.replies.length > 0){
-                let rep = blob.replies.map(m => formatPopoverComment(m))
-                starter += "-----------------------------" + rep;
-            }
-            return starter
-        }else{
-            return formatPopoverEmText(blob)
-        }
-      }
-
-      const formatPopoverComment = (blob)=> {
-        return `${anonName(blob["author"]["displayName"], 'initials')} (${anonName(blob["author"]["displayName"], 'role')}): "${blob["content"]}"`
-      }
-
-      const formatPopoverEmText = (blob) => {
-       
-        let blobIndex = blob['index-in'];
-        // let before = (blobIndex > 1 && state.projectData.googleData[blob['google-id']].body.content[blobIndex - 1].paragraph) ? state.projectData.googleData[blob['google-id']].body.content[blobIndex - 1].paragraph.elements : " ";
-        // let after = (state.projectData.googleData[blob['google-id']].body.content[blobIndex + 1] && state.projectData.googleData[blob['google-id']].body.content[blobIndex + 1].paragraph) ? state.projectData.googleData[blob['google-id']].body.content[blobIndex + 1].paragraph.elements : " ";
-        // return `${before[0].textRun} .... ${after.at(-1).textRun}`
-      }
-      
-      const formatEmphasis = (blob) => {
-          
-          let blobOb =  {margin: '4px'}
-          if(spanType === 'comment'){ 
-             
-              blobOb["fontWeight"] = "bold";
-              blobOb["backgroundColor"] = "rgb(253,213,145)";
-          }else{
-              if(blob['em']['textStyle'].bold === true) blobOb["fontWeight"] = "bold";
- 
-              if(blob['em']['textStyle'].backgroundColor) blobOb["backgroundColor"] = colorConvert(blob['em']['textStyle'].backgroundColor.color.rgbColor)
-              
-          }
-          return blobOb
-        }
-
-        const formatEmphasisVis = (blob) => {
-          
-          let blobOb =  {margin: '2px', width:'5px', height:'50px', backgroundColor: 'gray', display:"inline-block"}
-          if(spanType === 'comment'){ 
-              blobOb["fontWeight"] = "bold";
-              blobOb["backgroundColor"] = "rgb(253,213,145)";
-          }else{
-              if(blob['em']['textStyle'].bold === true) blobOb["width"] = '10px';
-              if(blob['em']['textStyle'].italic === true) blobOb["fontStyle"] = "italic";
-      
-              if(blob['em']['textStyle'].backgroundColor) {
-                blobOb["backgroundColor"] = colorConvert(blob['em']['textStyle'].backgroundColor.color.rgbColor)
-                blobOb["borderColor"] = "gray";
-                blobOb["borderStyle"] = "solid";
-                blobOb["borderWidth"] = '1px';
-              }
-              if(blob['em']['textStyle'].foregroundColor){ 
-                
-                blobOb["backgroundColor"] = colorConvert(blob['em']['textStyle'].foregroundColor.color.rgbColor);
-                blobOb["borderColor"] = "gray";
-                blobOb["borderStyle"] = "solid";
-                blobOb["borderWidth"] = '1px';
-              
-              }
-          }
-          return blobOb
-        }
 
         if (!showPopover){
           return (
             <Box onMouseEnter={() => setShowPopover(true)}>
-              { spanType === 'comment' ?  renderPop(data)   :   <>
+              { spanType === 'comment' ?  renderPop(data, spanType, anonName)   :   <>
                   <span>{formatContext(data, 'before')}</span>
-                  <span style={formatEmphasis(data)}>{formatText(data)}</span>
+                  <span style={formatEmphasis(data, spanType)}>{formatText(data, spanType)}</span>
                   <span>{formatContext(data, 'after')}</span>
                 </> }
             </Box>
@@ -143,7 +137,7 @@ import { format } from 'path/posix';
       return (
         <Popover isOpen={showPopover} onClose={closePopover}>
           <PopoverTrigger>
-            <div style={formatEmphasisVis(data)}></div>
+            <div style={formatEmphasisVis(data, spanType)}></div>
           </PopoverTrigger>
           <PopoverContent bg="white" color="gray">
             <PopoverHeader fontWeight="semibold">
@@ -153,11 +147,11 @@ import { format } from 'path/posix';
 
             <PopoverBody>
               {spanType === 'comment' ? (
-                <Box>{renderPop(data)}</Box>
+                <Box>{renderPop(data, spanType, anonName)}</Box>
               ) : (
                 <Box>
                   <span>{formatContext(data, 'before')}</span>
-                  <span style={formatEmphasis(data)}>{formatText(data)}</span>
+                  <span style={formatEmphasis(data, spanType)}>{formatText(data, spanType)}</span>
                   <span>{formatContext(data, 'after')}</span>
                 </Box>
               )}
@@ -170,5 +164,5 @@ import { format } from 'path/posix';
       );
 
   }
-  
+
   export default PopComment;
