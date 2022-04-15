29cd063f3ce9f6810c90969883fb242dea27ec9c
Jen Rogers <jen@sci.utah.edu>
2022-04-15 08:33:08 -0600

    save eml parser works

--- src/components/EmailRender.tsx
+++ src/components/EmailRender.tsx
@@ -1,42 +1,76 @@
 import React, { useState } from 'react';
 import { useProjectState } from './ProjectContext';
+import * as fs from 'fs';
+import path from 'path';
+const  EmlParser = require('eml-parser');
+
 
 const EmailRender = (props: any) => {
   const { title, setFragSelected } = props;
 
   const [state] = useProjectState();
-  const [emailData, setEmailData] = useState(['']);
+  const [emailData, setEmailData] = useState('');
 
-  const sendToFlask = async () => {
-    const response = await fetch(
-      `http://127.0.0.1:5000/parse_eml/${title}/${state.projectData.title}`
-    );
-    const newData = await response.text();
-    setEmailData(newData.split('>').filter((f) => f != ''));
-  };
+  // const sendToFlask = async () => {
+  //   const response = await fetch(
+  //     `http://127.0.0.1:5000/parse_eml/${title}/${state.projectData.title}`
+  //   );
+  //   const newData = await response.text();
+  //   setEmailData(newData.split('>').filter((f) => f != ''));
+  // };
 
-  sendToFlask();
+  // sendToFlask();
+
+  new EmlParser(fs.createReadStream(`${path.join(state.folderPath, title)}`))
+.parseEml()
+.then(result  => {
+	// properties in result object:
+	// {
+	//	"attachments": [],
+	//	"headers": {},
+	//	"headerLines": [],
+	//	"html": "",
+	//	"text": "",
+	//	"textAsHtml": "",
+	//	"subject": "",
+	//	"references": "",
+	//	"date": "",
+	//	"to": {},
+	//	"from": {},
+	//	"cc": {},
+	//	"messageId": "",
+	//	"inReplyTo": ""
+	// }
+	console.log('RESULT',result.html);
+  setEmailData(result.html)
+})
+.catch(err  => {
+	console.log(err);
+})
 
   return (
-    <div style={{ height: '90%', overflow: 'auto' }}>
-      {emailData.map((m, i) => (
-        <div
-          key={`email-${i}`}
-          style={{ display: 'block' }}
-          onMouseUp={() => {
-            if (setFragSelected) {
-              const selObj = window.getSelection();
-              console.log('test', selObj);
-              setFragSelected(selObj?.toString());
-            } else {
-              console.log('mouseup');
-            }
-          }}
-        >
-          {m}
-        </div>
-      ))}
-    </div>
+    <div 
+    dangerouslySetInnerHTML={{__html: emailData}}
+    />
+    // <div style={{ height: '90%', overflow: 'auto' }}>
+    //   {emailData.map((m, i) => (
+    //     <div
+    //       key={`email-${i}`}
+    //       style={{ display: 'block' }}
+    //       onMouseUp={() => {
+    //         if (setFragSelected) {
+    //           const selObj = window.getSelection();
+    //           console.log('test', selObj);
+    //           setFragSelected(selObj?.toString());
+    //         } else {
+    //           console.log('mouseup');
+    //         }
+    //       }}
+    //     >
+    //       {m}
+    //     </div>
+    //   ))}
+    // </div>
   );
 };
 
