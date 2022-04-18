02fca465d42d1ef0a507772002a67defc639f569
Jen Rogers <jen@sci.utah.edu>
2022-04-18 11:09:36 -0600

    save fileUtil.js

---
+++ src/fileUtil.js
@@ -0,0 +1,42 @@
+import { ipcRenderer } from 'electron';
+
+const os = require('os');
+const fs = require('fs-extra');
+
+
+export const openFile = (fileName, folderPath) => {
+    console.log('Open file:', path.join(folderPath, fileName));
+    ipcRenderer.send('open-file', path.join(folderPath, fileName));
+};
+
+export const readFile = (fileName) => new Promise((resolve, reject) => {
+    fs.readFile(fileName, (err, content) => {
+        if(err) return reject(err)
+
+        resolve(content)
+    })
+})
+
+export const writeFile = (fileName, content) => new Promise((resolve, reject) => {
+    console.log('write file');
+    fs.writeFile(fileName, content, err => {
+        if(err) return reject(err)
+
+        resolve()
+    })
+})
+
+export function decode(input) {
+    if(!input)
+        return ''
+    const decodedBase64 = atob(input.replace(/-/g, '+').replace(/_/g, '/'))
+    const escaped = escape(decodedBase64)
+    return decodeURIComponent(escaped)
+}
+
+export function decodeAttachment(input) {
+    if(!input)
+        return ''
+
+    return atob(encodeURI(input.replace(/-/g, '+').replace(/_/g, '/')))
+}
