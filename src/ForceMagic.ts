f8fe64ff64aa8bdca8a80d7fed03ec91f820b688
Jen Rogers <jen@sci.utah.edu>
2022-05-01 10:41:56 -0600

    filtered out missing files

--- src/ForceMagic.ts
+++ src/ForceMagic.ts
@@ -2,6 +2,67 @@ import * as d3 from 'd3';
 import { extent } from 'd3-array';
 import { scaleTime } from 'd3-scale';
 
+// force.on("tick", function(e) {
+//   var q,
+//     node,
+//     i = 0,
+//     n = nodes.length;
+
+//   var q = d3.geom.quadtree(nodes);
+
+//   while (++i < n) {
+//     node = nodes[i];
+//     q.visit(collide(node));
+//     xerr = node.x - node.true_x;
+//     yerr = node.y - node.true_y;
+//     node.x -= xerr*0.005;
+//     node.y -= yerr*0.9;
+//   }
+
+//   svg.selectAll("circle")
+//       .attr("cx", function(d) { return d.x; })
+//       .attr("cy", function(d) { return d.y; });
+// });
+
+// function collide(node) {
+//   var r = node.radius,
+//     nx1,
+//     nx2,
+//     ny1,
+//     ny2,
+//     xerr,
+//     yerr;
+
+//   nx1 = node.x - r;
+//   nx2 = node.x + r;
+//   ny1 = node.y - r;
+//   ny2 = node.y + r;
+
+//   return function(quad, x1, y1, x2, y2) {
+//     if (quad.point && (quad.point !== node)) {
+//       var x = node.x - quad.point.x,
+//           y = node.y - quad.point.y,
+//           l = Math.sqrt(x * x + y * y),
+//           r = node.radius + quad.point.radius;
+//       if (l < r) {
+//         // we're colliding.
+//         var xnudge, ynudge, nudge_factor;
+//         nudge_factor = (l - r) / l * .4;
+//         xnudge = x*nudge_factor;
+//         ynudge = y*nudge_factor;
+//         node.x -= xnudge;
+//         node.y -= ynudge;
+//         quad.point.x += xnudge;
+//         quad.point.y += ynudge;
+//       }
+//     }
+//     return x1 > nx2
+//         || x2 < nx1
+//         || y1 > ny2
+//         || y2 < ny1;
+//   };
+// }
+
 class ForceMagic {
   circleScale: any;
   yScale: any;
@@ -28,7 +89,9 @@ class ForceMagic {
       .range([0, height - this.margin])
       .domain(extent(projectEntries.map((e: any) => new Date(e.date))));
 
-    this.nodes = projectEntries.map((a: any) => {
+    let filtered = projectEntries.filter(f => f.files && f.files.length > 0);
+
+    this.nodes = filtered.map((a: any) => {
       const node = {};
       node.date = a.date;
       node.description = a.description;
