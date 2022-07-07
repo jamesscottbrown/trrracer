7c954bc331fe67d8313cf59ec90868b7a050810f
Jen Rogers <jen@sci.utah.edu>
2022-07-07 06:52:28 -0600

    removed commented out code in query comp

--- src/components/QueryView.tsx
+++ src/components/QueryView.tsx
@@ -124,35 +124,7 @@ const QueryView = (props: any) => {
             {m.googMatch.length > 0 &&
               m.googMatch.map((gm, j) => (
                 <QueryMatchComponent m={m} tm={gm} setViewType={setViewType} j={j} />
-                // <div style={{ marginTop: 10 }} key={`gm-${j}`}>
-                //   <HoverTitle
-                //     title={gm.title}
-                //     entry={m.entry}
-                //     match={gm}
-                //     setViewType={setViewType}
-                //   />
-                //   <div>
-                //     {gm.context.map((c, k) => (
-                       
-                //       <div key={`in-gm-${k}`}>
-                //         {c.map((m, l) => (
-                //           <span
-                //             key={`span-${l}`}
-                //             style={{
-                //               fontSize: 11,
-                //               fontWeight: m.style ? 700 : 400,
-                //               fontStyle: 'italic',
-                //               backgroundColor: m.style ? 'yellow' : '#ffffff',
-                //             }}
-                //           >
-                //             {m.context}
-                //             {'. '}
-                //           </span>
-                //         ))}
-                //       </div>
-                //     ))}
-                //   </div>
-                // </div>
+               
               ))}
           </div>
         ))}
