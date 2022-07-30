c05f61fd876c9bbcdbb3eba2b51ce7fa14f15a8a
Jen Rogers <jen@sci.utah.edu>
2022-07-30 14:36:48 -0600

    remove entryIndex

--- src/components/Entry.tsx
+++ src/components/Entry.tsx
@@ -308,7 +308,6 @@ const Entry = (props: EntryPropTypes) => {
           dispatch({
             type: 'ADD_TAG_TO_ENTRY',
             newTag: tag,
-            entryIndex,
             activityID: thisEntry.activity_uid,
           });
         }}
