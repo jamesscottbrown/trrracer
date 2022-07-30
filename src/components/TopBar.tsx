1bd832c0b405add9ffa23fef08ed7277e9457010
James Scott-Brown <james@jamesscottbrown.com>
2022-07-30 20:38:01 +0100

    correctly clear cookie value to navigate back to splash page

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -100,7 +100,7 @@ const TopBar = (ProjectPropValues: TopbarProps) => {
               <BiLogOut 
                 style={{display:'inline'}}
                 onClick={() => {
-                  document.cookie = 'folderPath=; Expires=Thu, 01 Jan 1970 00:00:01 GMT;'
+                  document.cookie = 'folderName=; Expires=Thu, 01 Jan 1970 00:00:01 GMT;'
                   console.log(document.cookie)
                   setPath('')}}
               />{" "}
