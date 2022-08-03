25790ae7dbbb2b1b3e0cab52477af6ba1992aca5
Jen Rogers <jen@sci.utah.edu>
2022-08-03 10:38:00 -0600

    remove unused function and imports

--- src/components/QueryView.tsx
+++ src/components/QueryView.tsx
@@ -1,80 +1,8 @@
 import React, { useState } from 'react';
-
-import {
-  Button,
-  Popover,
-  PopoverTrigger,
-  PopoverContent,
-  PopoverArrow,
-  PopoverBody,
-} from '@chakra-ui/react';
-
 import { MdCancel } from 'react-icons/md';
 import { useProjectState } from './ProjectContext';
 import QueryMatchComponent from './QueryMatchComponent';
 
-const HoverTitle = (props: any) => {
-  const { title, entry, match, setViewType } = props;
-
-  const [{ query }, dispatch] = useProjectState();
-
-  const fileIndex =
-    match.fileType === 'gdoc'
-      ? entry.files.map((m) => m.title).indexOf(match.title)
-      : match['file-index'];
-
-  const [showPopover, setShowPopover] = useState(false);
-
-  const closePopover = () => {
-    setShowPopover(false);
-  };
-
-  return (
-    <>
-      {showPopover ? (
-        <Popover isOpen={showPopover} onClose={closePopover}>
-          <PopoverTrigger>
-            <div>{title}</div>
-          </PopoverTrigger>
-          <PopoverContent bg="white" color="gray">
-            <PopoverArrow bg="white" />
-            <PopoverBody>
-              <Button
-                onClick={() => {
-                  console.log('matches??', match);
-                  setViewType('detail view');
-                  dispatch({
-                    type: 'SELECTED_ARTIFACT',
-                    activity: entry,
-                    artifactIndex: fileIndex,
-                    hopselArray: [
-                      {
-                        activity: entry,
-                        artifactUid: entry.files[fileIndex]
-                          ? entry.files[fileIndex].artifact_uid
-                          : null,
-                        hopReason: 'first hop',
-                      },
-                    ],
-                  });
-                  dispatch({
-                    type: 'UPDATE_GO_BACK',
-                    goBackView: 'query',
-                    filterQuery: query.matches.map((m) => m.entry.title),
-                  });
-                }}
-              >
-                See artifact in detail.
-              </Button>
-            </PopoverBody>
-          </PopoverContent>
-        </Popover>
-      ) : (
-        <div onMouseEnter={() => setShowPopover(true)}>{title}</div>
-      )}
-    </>
-  );
-};
 
 interface QueryViewProps {
   setViewType: (viewType: string) => void;
@@ -92,7 +20,7 @@ const QueryView = (props: QueryViewProps) => {
         style={{
           padding: 5,
           position: 'absolute',
-          top: '-20px',
+          top: '0px',
           right: '10px',
           zIndex: '1000',
           cursor: 'pointer',
@@ -118,22 +46,31 @@ const QueryView = (props: QueryViewProps) => {
 
               {m.textMatch.length > 0 &&
                 m.textMatch.map((tm, j) => (
-                  <QueryMatchComponent
-                    m={m}
-                    tm={tm}
-                    setViewType={setViewType}
-                    j={j}
-                  />
+                  <React.Fragment
+                  key={`rf-${j}`}
+                  >
+                    <QueryMatchComponent
+                      m={m}
+                      tm={tm}
+                      setViewType={setViewType}
+                      j={j}
+                    />
+                  </React.Fragment>
+               
                 ))}
 
               {m.googMatch.length > 0 &&
                 m.googMatch.map((gm, j) => (
+                  <React.Fragment
+                  key={`gf-${j}`}
+                  >
                   <QueryMatchComponent
                     m={m}
                     tm={gm}
                     setViewType={setViewType}
                     j={j}
                   />
+                  </React.Fragment>
                 ))}
             </div>
           ))}
