379a84680886524f9070dfaa98ed61982ccd2fa1
Jen Rogers <jen@sci.utah.edu>
2022-08-07 20:01:54 -0600

    fixed map error in the email render

--- src/components/EmailRender.tsx
+++ src/components/EmailRender.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import * as Showdown from 'showdown';
 import { readFileSync } from '../fileUtil';
+import { useProjectState } from './ProjectContext';
 // import * as fs from 'fs';
 // import path from 'path';
 
@@ -15,7 +16,7 @@ const EmailRender = (props: any) => {
     tasklists: true,
   });
 
-  const [emailData, setEmailData] = useState('Email failed to load');
+  const [emailData, setEmailData] = useState<null|any>(null);
 
   useEffect(() => {
     readFileSync(`${folderPath}/${title}`).then((eml) => {
@@ -38,7 +39,7 @@ const EmailRender = (props: any) => {
     // style={{ height: '95%', overflow: 'auto' }}
     // dangerouslySetInnerHTML={{__html: emailData}}
     // />
-    emailData && (
+    emailData ? (
       <div
         style={{
           width: '700px',
@@ -48,11 +49,13 @@ const EmailRender = (props: any) => {
         }}
       >
         {/* <div dangerouslySetInnerHTML={{__html: emailData}} /> */}
-        {emailData.map((em) => (
-          <div>{em}</div>
+        {emailData.map((em, i) => (
+          <div
+          key={`email-${i}`}
+          >{em}</div>
         ))}
       </div>
-    )
+    ) : "Email failed to load."
   );
 };
 
